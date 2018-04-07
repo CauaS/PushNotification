c506faffd2b264635a31b24e34835d76c14e3a5d
CauaS <37887533+CauaS@users.noreply.github.com>
2018-04-07 16:45:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+import App from './src/components/App.js';
+
+const pushNot = props => {
+    <App />
+}
+
+AppRegistry.registerComponent('pushNot', () => App);
